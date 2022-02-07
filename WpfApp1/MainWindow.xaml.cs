3137f6d2ba8afad8e7c5e09848660d04ecdd10ab
HowdareyouBass <tf2kub1k@gmail.com>
2022-02-07 11:36:56 +0300

    '1st'

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TheBall ball = new TheBall();
+        Stopwatch stopWatch = new Stopwatch();
+        int framecounter;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Stopwatch stopWatch = new Stopwatch();
+            DrawBall(104,104);
+            DrawBorder();
+            CompositionTarget.Rendering += UpdateFrame;
+        }
+
+        protected void UpdateFrame(object sender, EventArgs e)
+        {
+            if(framecounter == 0)
+            {
+                stopWatch.Start();
+            }
+            long framerate = (long)(framecounter / this.stopWatch.Elapsed.TotalSeconds);
+            DrawFrame(stopWatch.Elapsed);
+        }
+
+        private void DrawBall(double X1,double Y1)
+        {
+            Ellipse ball = new Ellipse()
+            {
+                Stroke = Brushes.Green,
+                Fill = Brushes.Green,
+                Width = 25,
+                Height = 25
+            };
+            ball.Margin = new Thickness(X1, Y1, 0, 0);
+            Field.Children.Add(ball);
+        }
+
+        private void DrawBorder()
+        {
+            Rectangle border = new Rectangle() {
+                Stroke = Brushes.LightGray,
+                StrokeThickness = 4,
+                Width = Field.Width-8,
+                Height = Field.Height-8
+            };
+            Field.Children.Add(border);
+        }
+        private void DrawFrame(TimeSpan tsa)
+        {
+            Field.Children.Clear();
+            DrawBorder();
+            SetPoints();
+            CheckCollision();
+            ball.velocity = Vector.Add(Vector.Multiply(tsa.TotalSeconds, ball.acceleration), ball.velocity);
+            ball.position = Vector.Add(Vector.Multiply(tsa.TotalSeconds, ball.velocity), ball.position);
+            DrawBall(ball.position.X, ball.position.Y);
+            DrawVectors();
+        }
+        private void SetPoints()
+        {
+            ball.leftPoint = new Point(ball.position.X, ball.position.Y + 25 / 2);
+            ball.rightPoint = new Point(ball.position.X + 25, ball.position.Y + 25 / 2);
+            ball.topPoint = new Point(ball.position.X + 25 / 2, ball.position.Y);
+            ball.bottomPoint = new Point(ball.position.X + 25 / 2, ball.position.Y + 25);
+        }
+        private void CheckCollision()
+        {
+            if(ball.leftPoint.X < 8 || ball.rightPoint.X > Field.Width - 8)
+            {
+                ball.velocity = new Vector(-ball.velocity.X, ball.velocity.Y);
+            }
+            if(ball.bottomPoint.Y > Field.Height - 8 || ball.topPoint.Y < 8)
+            {
+                ball.velocity = new Vector(ball.velocity.X, -ball.velocity.Y);
+            }
+        }
+        private void DrawVectors()
+        {
+            Line acc = new Line()
+            {
+                Stroke = Brushes.Red,
+                X1 = ball.position.X + 25 / 2,
+                Y1 = ball.position.Y + 25 / 2,
+                X2 = ball.position.X + 25 / 2 + ball.acceleration.X / 4,
+                Y2 = ball.position.Y + 25 / 2 + ball.acceleration.Y / 4
+            };
+            Line vel = new Line()
+            {
+                Stroke = Brushes.Yellow,
+                X1 = ball.position.X + 25 / 2,
+                Y1 = ball.position.Y + 25 / 2,
+                X2 = ball.position.X + 25 / 2 + ball.velocity.X / 4,
+                Y2 = ball.position.Y + 25 / 2 + ball.velocity.Y / 4
+            };
+            Field.Children.Add(acc);
+            Field.Children.Add(vel);
+        }
+    }
+}
---
+++ WpfApp1/TheBall.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    class TheBall
+    {
+        public Vector velocity = new Vector(300,0);
+        public Vector acceleration = new Vector(0,700);
+        public Point position = new Point(204, 204);
+        public Point leftPoint = new Point(),
+                     topPoint = new Point(),
+                     rightPoint = new Point(),
+                     bottomPoint = new Point();
+    }
+}
